0cbe8fbe7edbd01e1a0f2db106588514efbd6ff1
sdoesinger <sdoesinger@MacBook-Air.local>
2025-06-21 06:51:37 +0200

    fix: final working GitHub Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
-  base: "/doesinger-app/",
+  base: '/doesinger-app/',
+  build: {
+    outDir: 'docs',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [react()],
-});
-
+})
 
